6edc213392740acfb3d75c42d8a44ce92f5b6ed7
axelobscura <axosar@gmail.com>
2023-09-24 15:33:20 -0600

    gastos

---
+++ app/gastos/page.tsx
@@ -0,0 +1,9 @@
+export default function Gastos() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className='text-xl'>SERES DE RIQUEZA</h1>
+        <h2>GASTOS PERSONALES</h2>
+        <hr className='mt-3 mb-3'/>
+      </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className='text-xl'>SERES DE RIQUEZA</h1>
-      <h2>FINANZAS PERSONALES</h2>
+      <h2><Link href='/'>FINANZAS PERSONALES</Link> | <Link href='/gastos'>GASTOS PERSONALES</Link></h2>
       <hr className='mt-3 mb-3'/>
       <div className='justify-left w-full'>
         <h3 className='text-xl mb-5 border-b border-gray-300'>INGRESOS</h3>
@@ -50,7 +50,7 @@ export default function Home() {
           <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
         </div>
       </div>
-      <button className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 text-xl">
+      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-5 text-xl">
         CALCULAR
       </button>
         
