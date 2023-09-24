2f4371e1c3f90c34a5fd2e74b838ad7f70d234e6
axelobscura <axosar@gmail.com>
2023-09-24 13:33:42 -0600

    forma

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,22 @@ export default function Home() {
       <h1 className='text-xl'>SERES DE RIQUEZA</h1>
       <h2>FINANZAS PERSONALES</h2>
       <hr className='mt-3 mb-3'/>
-      <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full mt-3"></input>
+      <div className='justify-left w-full'>
+        <h3 className='text-lg mb-5'>INGRESOS</h3>
+        <h2>EMPLEO</h2>
+        <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
+        <h2 className='mt-3'>ADICIONALES</h2>
+        <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
+        <h2 className='mt-3'>COMISIONES</h2>
+        <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
+        <h2 className='mt-3'>VENTA DE ACTIVOS</h2>
+        <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
+        <h2 className='mt-3'>INGRESOS POR INVERSIONES</h2>
+        <input type="text" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
+      </div>
+      
+      <hr className='mt-3 mb-3'/>
+      
     </main>
   )
 }
