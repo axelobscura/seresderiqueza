5f2596e9dbf6fcb766d6ba7b629b1cddef3f13fa
axelobscura <axosar@gmail.com>
2023-09-24 15:44:17 -0600

    gastos

--- app/gastos/page.tsx
+++ app/gastos/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link"
+
 export default function Gastos() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <h1 className='text-xl'>SERES DE RIQUEZA</h1>
-        <h2>GASTOS PERSONALES</h2>
+        <h2><Link href='/' className='text-gray-500 hover:text-gray-700'>FINANZAS PERSONALES</Link> | <Link href='/gastos' className='text-gray-500 hover:text-gray-700'>GASTOS PERSONALES</Link></h2>
         <hr className='mt-3 mb-3'/>
+        <div className='justify-left w-full'>
+            <h3 className='text-xl mb-5 border-b border-gray-300'>GASTOS</h3>
+        </div>
       </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className='text-xl'>SERES DE RIQUEZA</h1>
-      <h2><Link href='/'>FINANZAS PERSONALES</Link> | <Link href='/gastos'>GASTOS PERSONALES</Link></h2>
+      <h2><Link href='/' className='text-gray-500 hover:text-gray-700'>FINANZAS PERSONALES</Link> | <Link href='/gastos' className='text-gray-500 hover:text-gray-700'>GASTOS PERSONALES</Link></h2>
       <hr className='mt-3 mb-3'/>
       <div className='justify-left w-full'>
         <h3 className='text-xl mb-5 border-b border-gray-300'>INGRESOS</h3>
