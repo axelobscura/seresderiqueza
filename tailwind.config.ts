ecb578ac7682295e6f7eaed6ff972e5409f4c6fb
axelobscura <axosar@gmail.com>
2023-09-24 13:17:43 -0600

    inout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        'xs': '.75rem',   // Extra-small font size
+        'sm': '.875rem',  // Small font size
+        'base': '1rem',   // Base font size
+        'lg': '2rem', // Large font size
+        'xl': '2.25rem',  // Extra-large font size
+        '2xl': '3.5rem',  // Double extra-large font size
+          // Add more custom font sizes if needed
+      },
     },
   },
   plugins: [],
