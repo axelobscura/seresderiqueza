d6d0d5c1e2ce1dbb445a86bcb4ee295f0b0ad5ba
axelobscura <axosar@gmail.com>
2023-10-12 22:07:15 -0600

    icons

--- app/page.tsx
+++ app/page.tsx
@@ -90,6 +90,9 @@ export default function Home() {
               content="Content for Item 3"
             />
           </div>
+          <button className="bg-purple-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-5 text-xl w-full">
+            CALCULAR
+          </button>
         </div>
       </div>
 
