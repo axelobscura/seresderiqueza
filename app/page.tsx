a40b783beafc6b0605fac8b81c39e96cfd01a16d
axelobscura <axosar@gmail.com>
2023-10-20 21:32:59 -0600

    fondos

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import Accordion from "./components/Accordion"
 import { RocketLaunchIcon, MegaphoneIcon, PlusIcon } from '@heroicons/react/24/solid'
 
@@ -6,110 +7,22 @@ import { RocketLaunchIcon, MegaphoneIcon, PlusIcon } from '@heroicons/react/24/s
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className='text-xl text-center'>SERES DE RIQUEZA</h1>
-      <h2><Link href='/' className='text-gray-500 hover:text-gray-700 text-center'>FINANZAS PERSONALES</Link> | <Link href='/gastos' className='text-gray-500 hover:text-gray-700'>GASTOS PERSONALES</Link></h2>
-      <hr className='mt-3 mb-3' />
-
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="sm:p12 md:p-4">
-          <div className='justify-left w-full'>
-            <h3 className='flex flex-row items-center text-xl mb-5 border-b border-gray-300'><RocketLaunchIcon className="h-10 w-10 text-blue-500 mr-2 bg-gray-900 p-2" /> FINANZAS PERSONALES</h3>
-          </div>
-          <div className='grid sm:grid-cols-1 gap-3 md:grid-cols-3 w-full'>
-            <div className='w-full'>
-              <h3 className='flex flex-row items-center text-base bg-gray-900 p-3 mb-3'><PlusIcon className="h-10 w-10 text-blue-500 mr-2 bg-gray-900 p-2" /> REAL MES</h3>
-              <h2>EMPLEO</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>ADICIONALES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>COMISIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>VENTA DE ACTIVOS</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>INVERSIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-            </div>
-            <div className='w-full'>
-              <h3 className='flex flex-row items-center text-base bg-gray-900 p-3 mb-3'><PlusIcon className="h-10 w-10 text-blue-500 mr-2 bg-gray-900 p-2" /> PRESUPUESTO</h3>
-              <h2>EMPLEO</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>ADICIONALES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>COMISIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>VENTA DE ACTIVOS</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>INVERSIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-            </div>
-            <div className='w-full'>
-              <h3 className='flex flex-row items-center text-base bg-gray-900 p-3 mb-3'><PlusIcon className="h-10 w-10 text-blue-500 mr-2 bg-gray-900 p-2" /> OBJECTIVO %</h3>
-              <h2>EMPLEO</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>ADICIONALES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>COMISIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>VENTA DE ACTIVOS</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-              <h2 className='mt-3'>INVERSIONES</h2>
-              <input type="number" className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-gray-500 w-full mt-3 text-gray-900"></input>
-            </div>
-          </div>
-          <button className="bg-purple-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-5 text-xl w-full">
-            CALCULAR
-          </button>
-        </div>
-        <div className="p-4">
-          <div className='justify-left w-full'>
-            <h3 className='flex flex-row items-center text-xl mb-5 border-b border-gray-300'><MegaphoneIcon className="h-10 w-10 text-blue-500 mr-2 bg-gray-900 p-2" /> GASTOS PERSONALES</h3>
-          </div>
-          <div>
-            <Accordion
-              title="GASTOS FIJOS"
-              content="Content for Item 1"
-            />
-            <Accordion
-              title="DEUDAS"
-              content="Content for Item 2"
-            />
-            <Accordion
-              title="GASTOS AJUSTABLES"
-              content="Content for Item 3"
-            />
-            <Accordion
-              title="GASTOS POR GUSTO"
-              content="Content for Item 3"
-            />
-            <Accordion
-              title="AHORROS E IMPREVISTOS"
-              content="Content for Item 3"
-            />
-            <Accordion
-              title="PROYECCIONES DE AHORROS"
-              content="Content for Item 3"
-            />
-          </div>
-          <button className="bg-purple-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-5 text-xl w-full">
-            CALCULAR
-          </button>
-        </div>
+      <Image
+        src='/logo.webp'
+        width='250'
+        height='250'
+        alt="una comunidad de inversionistas y educadores"
+      />
+      <p className="font-play text-white text-center">Somos una comunidad de inversionistas y educadores,<br/>nuestra misión es transformar vidas<br/>a través de la educación financiera y ayudar a las personas a alcanzar su plenitud financiera.</p>
+      <h1 className="font-play text-white text-center mt-5 text-bold">CALCULADORA FINANCIERA PERSONAL</h1>
+      <div className="grid grid-cols-2 gap-4 mt-10">
+        <Link href='/finanzas' className="p-5 bg-oro text-azul hover:bg-gray-900 hover:text-white">
+          <p className="text-center">FINANZAS PERSONALES</p>
+        </Link>
+        <Link href='/' className="p-5 bg-azuldos text-white hover:bg-gray-900">
+          <p className="text-center">GASTOS PERSONALES</p>
+        </Link>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <hr className='mt-3 mb-3' />
-
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,10 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        play: ["Quicksand", "sans-serif"],
       },
       fontSize: {
         'xs': '.75rem',   // Extra-small font size
@@ -22,6 +24,12 @@ const config: Config = {
         '2xl': '3.5rem',  // Double extra-large font size
           // Add more custom font sizes if needed
       },
+      colors: {
+        'oro': '#d8c590',
+        'orodos': '#a7986c',
+        'azul': '#1b3850',
+        'azuldos': '#53a1be',
+      }
     },
   },
   plugins: [],
